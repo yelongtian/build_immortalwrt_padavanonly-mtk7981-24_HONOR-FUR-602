242d02e432d291da4b721d815e63a5f48ebade0e
yelongtian <yelongtian@github.com>
2026-07-01 20:07:37 +0800

    fix passwall2 feed pull failure: add .git;main suffix

--- doc/diy/diy-fur602-hanwckf-1.sh
+++ doc/diy/diy-fur602-hanwckf-1.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/Zxilly/UA2F package/UA2F
 
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
